27e46a43110c1d99d2b5788dbded909a07579580
happyjy <okwoyjy@gmail.com>
2022-12-04 11:16:46 +0900

    redux middleware에 redux-saga를 추가하는 방법
      - Hellow Sagas문구를 console log로 남김

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
-import "@babel/polyfill"
+import "@babel/polyfill";
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
 
-import Counter from './Counter'
-import reducer from './reducers'
+import Counter from "./Counter";
+import reducer from "./reducers";
 
-const store = createStore(reducer)
+import { helloSaga } from "./sagas";
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+sagaMiddleware.run(helloSaga);
 
-const action = type => store.dispatch({type})
+const action = (type) => store.dispatch({ type });
+
+// const store = createStore(reducer);
+// const action = (type) => store.dispatch({ type });
 
 function render() {
   ReactDOM.render(
     <Counter
       value={store.getState()}
-      onIncrement={() => action('INCREMENT')}
-      onDecrement={() => action('DECREMENT')} />,
-    document.getElementById('root')
-  )
+      onIncrement={() => action("INCREMENT")}
+      onDecrement={() => action("DECREMENT")}
+    />,
+    document.getElementById("root")
+  );
 }
 
-render()
-store.subscribe(render)
+render();
+store.subscribe(render);
